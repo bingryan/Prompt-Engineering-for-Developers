aac70fcea0223b75c267ab0c27a18fd0284ca42f
bingryan <bingryan@users.noreply.github.com>
2023-04-30 13:48:20 +0000

    deploy: b1e2522f01c0d1c5835353b33bdcf00c68a1ebae

---
+++ theme/js/custom.js
@@ -0,0 +1,69 @@
+var initAll = function () {
+
+	// https://github.com/JorelAli/mdBook-pagetoc/blob/master/sidebar.js
+    // Un-active everything when you click it
+    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+        el.addEventHandler("click", function () {
+            Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+                el.classList.remove("active");
+            });
+            el.classList.add("active");
+        });
+    });
+
+    var updateFunction = function () {
+        var id = null;
+        var elements = document.getElementsByClassName("header");
+        Array.prototype.forEach.call(elements, function (el) {
+            if (window.pageYOffset >= el.offsetTop) {
+                id = el;
+            }
+        });
+
+        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+            el.classList.remove("active");
+        });
+
+        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+            if (id == null) {
+                return;
+            }
+            if (id.href.localeCompare(el.href) == 0) {
+                el.classList.add("active");
+            }
+        });
+    };
+
+    var pagetoc = document.getElementsByClassName("pagetoc")[0];
+    var elements = document.getElementsByClassName("header");
+    Array.prototype.forEach.call(elements, function (el) {
+        var link = document.createElement("a");
+
+        // Indent shows hierarchy
+        var indent = "";
+        switch (el.parentElement.tagName) {
+            case "H1":
+                return;
+            case "H3":
+                indent = "20px";
+                break;
+            case "H4":
+                indent = "40px";
+                break;
+            default:
+                break;
+        }
+
+        link.appendChild(document.createTextNode(el.text));
+        link.style.paddingLeft = indent;
+        link.href = el.href;
+        pagetoc.appendChild(link);
+    });
+    updateFunction.call();
+
+    // Handle active elements on scroll
+    window.addEventListener("scroll", updateFunction);
+    
+};
+
+window.addEventListener('load', initAll);
